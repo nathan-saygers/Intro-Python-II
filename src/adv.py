b14b6435f5cf8487e16bc9eef34d1d671d99eb29
nathan-saygers <nathan.saygers@gmail.com>
2020-04-08 14:34:25 -0700

    updates player import, room direction attributes, and creates player object

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,24 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons",  'foyer', 'none', 'none', 'none'),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""",  'overlook', 'outside', 'narrow', 'none'),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""",  'none', 'foyer', 'none', 'none'),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""",  'treasure', 'none', 'none', 'foyer'),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""",  'none', 'narrow', 'none', 'none'),
 }
 
 
@@ -39,6 +39,10 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+davie = Player('Davie Jones', 'outside')
+
+print(room['treasure'])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +53,8 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+# player_input = input("Please enter a command:")
+
+# while player_input != 'q':
+#     print(davie[current_room])
