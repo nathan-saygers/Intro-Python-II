d2cea06c032984d7448a1d0adf61ebdc1df17357
nathan-saygers <nathan.saygers@gmail.com>
2020-04-08 14:35:16 -0700

    adds additional player attributes

--- src/player.py
+++ src/player.py
@@ -2,5 +2,8 @@
 # currently.
 
 class Player():
-  def __init__(self, currRoom):
-    self.currRoom = currRoom
+  def __init__(self, name, current_room):
+    self.name = name
+    self.current_room = current_room
+  def __str__(self):
+    return f"I am in {self.current_room}"
